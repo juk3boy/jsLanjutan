f778272d538a2e8574d52c546a46fd5babb5a295
Yudi <sumberdaya.travel@gmail.com>
2021-07-18 18:46:23 +0700

    update 18-07-2021 18:46

--- templateLiteral/script.js
+++ templateLiteral/script.js
@@ -87,16 +87,116 @@
 
 // kita bisa beri conditinals
 
-const lagu = {
-    judul: 'Maju',
-    penyanyi: 'Makmur',
-    // feat: 'bumi jalan'  /** bisa ada bisa enggak */
-}
+// const lagu = {
+//     judul: 'Maju',
+//     penyanyi: 'Makmur',
+//     // feat: 'bumi jalan'  /** bisa ada bisa enggak */
+// }
 
-const el = `<div class="lagu">
-<li>${lagu.penyanyi}</li>
+// const el = `<div class="lagu">
+// <li>${lagu.penyanyi}</li>
 
-<li>${lagu.judul} ${lagu.feat ? `(feat.${lagu.feat})` : ''}</li>
-</div>`
-document.body.innerHTML = el;
+// <li>${lagu.judul} ${lagu.feat ? `(feat.${lagu.feat})` : ''}</li>
+// </div>`
 
+
+/** NESTED
+ * HTML Fragments Bersarang
+ *
+ */
+
+// const mhs = {
+//     nama: 'Yudi',
+//     semesater: 5,
+//     mataKuliah: ['Rekayasa Web',
+//         'Analisis dan Perancangan Sistem Informasi',
+//         'Pemprograman Sistem Interaktif',
+//         'Perancangan Sistem Berorentasi Object']
+// };
+
+// function cetakMataKuliah(mataKuliah) {
+//     return `
+//     <ol>
+//       ${mataKuliah.map(mk => `<li> ${mk}</li>`).join('')}
+//     </ol >
+//         `;
+// }
+
+// const el = `<div div class= "mhs" >
+// <h2>${mhs.nama}</h2>
+// <span>Semester : ${mhs.semesater}</span>
+// <h4>Mata Kuliah :</h4>
+// ${cetakMataKuliah(mhs.mataKuliah)}
+// </div>`;
+// document.body.innerHTML = el;
+
+
+/** Akhir dari Nested */
+
+
+/** Tagged Templates
+ *
+ * Definisi dari MDN web docs
+ * Bentuk yang lebih kompleks dari Template Literals,
+ * memungkinkan kita untuk membaca templates literals melalu sebuah function
+ *
+ */
+
+// Tagged Templates
+
+// const nama = 'Yudi Kristianta';
+// const umur = 38;
+
+// function coba(strings, ...values) {
+//     // let result = '';
+//     // strings.forEach((str, i) => {
+//     //     result += `${str}${values[i] || ''}`;
+//     // });
+
+//     // return result;
+
+//     // diatas ini akan mencetak -> Halo nama saya Yudi Kristianta, saya berumur 38 tahun.
+
+//     /** dan juga bisa menggunakan cara dibawah ini */
+
+//     // return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}`, '');
+// }
+
+// const str = coba`Halo nama saya ${nama}, saya berumur ${umur} tahun.`;
+// console.log(str);
+
+
+// Jika ada Highlight di nama dan umur nya
+
+// const nama = 'Yudi Kristianta';
+// const umur = 38;
+// const email = 'yudi@yahoo.com';
+
+// function highlight(strings, ...values) {
+
+//     return strings.reduce((result, str, i) => `${result}${str}<span class="hl">${values[i] || ''}</span>`, '');
+// }
+
+// const str = highlight`Halo nama saya ${nama}, saya berumur ${umur} tahun, dan email saya ${email}`;
+
+// // console.log(str);
+
+// document.body.innerHTML = str;
+
+
+/**
+ * Contoh lain dari Tagged Templates
+ */
+
+/** Escaping / Sanitize HTML Tags */
+// function sanitize(strings, ...values) {
+//     return DOMPurify.sanitize(aboutMe);
+// }
+
+// const name = 'petyr bealish';
+// const aboutMe = `I Love to do evil <img src="http://unplash.it/100/100?random" onload="alert('I Hacked You . haha');" />`;
+
+// const html = sanitize`
+//     <h3> ${name}</h3>
+//     <p>${aboutMe}</p>
+//     `;
